f97776e872c1776a79797bd0eb56edb631bf1d50
josh <jlee12@ohiochristian.edu>
2025-06-08 23:58:09 +0800

    Add github workflows

--- main.tf
+++ main.tf
@@ -48,3 +48,23 @@ resource "aws_s3_bucket_policy" "public_read_policy" {
     ]
   })
 }
+
+resource "aws_s3_object" "site_files" {
+  for_each = fileset("app", "**")
+
+  bucket = aws_s3_bucket.cloud_resume_challenge.id
+  key    = each.value
+  source = "app/${each.value}"
+  etag   = filemd5("app/${each.value}")
+  content_type = lookup(
+    {
+      html = "text/html"
+      css  = "text/css"
+      js   = "application/javascript"
+      png  = "image/png"
+      jpg  = "image/jpeg"
+    },
+    split(".", each.value)[length(split(".", each.value)) - 1],
+    "application/octet-stream"
+  )
+}
