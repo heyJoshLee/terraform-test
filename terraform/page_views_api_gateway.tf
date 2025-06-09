15f631ea42e3a0bdb3be9c200cbe3515f6822f32
josh <jlee12@ohiochristian.edu>
2025-06-09 09:06:32 +0800

    folder structuure

--- terraform/page_views_api_gateway.tf
+++ terraform/page_views_api_gateway.tf
@@ -30,3 +30,15 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.page_view_api.id
   stage_name  = "prod"
 }
+
+
+resource "aws_api_gateway_rest_api" "page_view_api" {
+  name        = "PageViewAPI"
+  description = "API Gateway for page views"
+}
+
+resource "aws_api_gateway_resource" "page_view_resource" {
+  rest_api_id = aws_api_gateway_rest_api.page_view_api.id
+  parent_id   = aws_api_gateway_rest_api.page_view_api.root_resource_id
+  path_part   = "pageview"
+}
--- terraform/page_views_iam.tf
+++ terraform/page_views_iam.tf
@@ -1,3 +1,18 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
 
 data "aws_iam_policy_document" "lambda_dynamodb_policy" {
   statement {
